ddc07660eb519bd7ad1d413b3a0cbde31a9d9edf
TaoXu <taoxu1215@gmail.com>
2016-07-06 14:51:05 +0800

    adjust partitioned config

--- code/evaluate/analyse_result.py
+++ code/evaluate/analyse_result.py
@@ -3,12 +3,14 @@
 from result import Result
 
 def read():
-    with open('../../data/50.601088.result', 'r') as f:
+    with open('../../data/800core100result', 'r') as f:
         lines = f.readlines()
 
     result_list = []
     result_template = None
     ix = 0
+
+    test_winrate_05, test_pos_num_05, test_winrate_06, test_pos_num_06, test_winrate_07, test_pos_num_07, test_winrate_08, test_pos_num_08, test_winrate_09, test_pos_num_09, test_acc = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
     while ix < len(lines):
         line = lines[ix]
         if 'learning_rate' in line:
@@ -17,58 +19,72 @@ def read():
             strategy = tmp[1].split()[-1]
             punishment = float(tmp[2].split()[-1])
             result_template = Result(strategy, learning_rate, punishment)
-            ix += 2
+            ix += 3
         elif 'val' in line:
             val_winrate_05 = float(line.split()[-1])
             ix += 1
             val_pos_num_05 = float(lines[ix].split()[-1])
+
+            '''
             ix += 1
             test_winrate_05 = float(lines[ix].split()[-1])
             ix += 1
             test_pos_num_05 = float(lines[ix].split()[-1])
+            '''
 
             ix += 1
             val_winrate_06 = float(lines[ix].split()[-1])
             ix += 1
             val_pos_num_06 = float(lines[ix].split()[-1])
+
+            '''
             ix += 1
             test_winrate_06 = float(lines[ix].split()[-1])
             ix += 1
             test_pos_num_06 = float(lines[ix].split()[-1])
+            '''
 
             ix += 1
             val_winrate_07 = float(lines[ix].split()[-1])
             ix += 1
             val_pos_num_07 = float(lines[ix].split()[-1])
+            '''
             ix += 1
             test_winrate_07 = float(lines[ix].split()[-1])
             ix += 1
             test_pos_num_07 = float(lines[ix].split()[-1])
+            '''
 
             ix += 1
             val_winrate_08 = float(lines[ix].split()[-1])
             ix += 1
             val_pos_num_08 = float(lines[ix].split()[-1])
+            '''
             ix += 1
             test_winrate_08 = float(lines[ix].split()[-1])
             ix += 1
             test_pos_num_08 = float(lines[ix].split()[-1])
+            '''
 
             ix += 1
             val_winrate_09 = float(lines[ix].split()[-1])
             ix += 1
             val_pos_num_09 = float(lines[ix].split()[-1])
+            '''
             ix += 1
             test_winrate_09 = float(lines[ix].split()[-1])
             ix += 1
             test_pos_num_09 = float(lines[ix].split()[-1])
+            '''
 
             ix += 1
             epoch = int(lines[ix].split()[1])
             ix += 4
             val_acc = float(lines[ix].split()[2]) / 100
+            '''
             ix += 2
             test_acc = float(lines[ix].split()[2]) / 100
+            '''
 
             single_result = result_template.get_new_result(epoch,
                 val_winrate_05, val_pos_num_05, test_winrate_05, test_pos_num_05,
@@ -87,8 +103,8 @@ def read():
 if __name__ == '__main__':
     result_list = read()
     #filtering
-    result_list = filter(lambda item: item.val_pos_num_05 > 20,  result_list)
+    result_list = filter(lambda item: item.val_pos_num_05 > 40,  result_list)
 
-    result_list.sort(key=lambda item: (item.val_acc + item.test_acc), reverse=True)
-    for ix in xrange(10):
+    result_list.sort(key=lambda item: item.val_acc, reverse=True)
+    for ix in xrange(min(10, len(result_list))):
         print result_list[ix]
